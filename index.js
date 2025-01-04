9f1a34e47c815eb14b75df522fed8070d01aafe8
Jade Browne <jade.browne@outlook.com>
2025-01-03 20:38:51 -0500

    update CSS; code js with functioning clock

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-//digital clock program
-
-function updateClock(){
-
+function updateClock() {
+    const now = new Date();
+    // Make clock a 12-hour format
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, "0"); // First two characters padded with "0"
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
 }
+
+updateClock();
+setInterval(updateClock, 1000); // Update every 1000 ms
